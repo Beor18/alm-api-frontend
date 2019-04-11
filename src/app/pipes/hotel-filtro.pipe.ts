9b13bbfce852c4472a674ed0a0df9aa3f3c0fc6e
Beor18 <fer.ragnar19@gmail.com>
2019-04-11 19:51:59 -0300

    movido a la carpeta pipes

---
+++ src/app/pipes/hotel-filtro.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Hotels } from '../hotel/hotel';
+
+
+@Pipe({
+    name: 'hotelFiltro'
+})
+export class HotelFiltroPipe implements PipeTransform {
+
+    transform(value: Hotels[], filter: string): Hotels[] {
+        filter = filter ? filter.toLocaleLowerCase() : null;
+        return filter ? value.filter((movie: Hotels) =>
+            movie.name.toLocaleLowerCase().search(filter) !== -1) : value;
+    }
+
+    private handleMap(res: any, id: string) {
+        const data = <Hotels[]> res.json();
+        // Return an initialized object
+        if (id === '') {
+            return {
+                'name': ''
+            };
+        }
+        const filtered = data.filter(m => m._id === id);
+        return <Hotels> filtered[0];
+}
+}
